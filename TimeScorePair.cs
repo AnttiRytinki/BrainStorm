4546456d94de09ba73f99d3307cb9d8dd46c753e
Antti Rytinki <antti.rytinki@outlook.com>
2025-05-13 11:02:48 +0200

    Create User.cs Update ROADMAP

---
+++ TimeScorePair.cs
@@ -0,0 +1,8 @@
+﻿namespace BrainStorm
+{
+    public class TimeScorePair
+    {
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public int Score { get; set; } = -1;
+    }
+}

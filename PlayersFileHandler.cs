03deccb7b3390aedd0ffe52730b2da1f613289c9
Antti Rytinki <antti.rytinki@outlook.com>
2025-05-16 13:35:43 +0200

    Save and Load players and scores

--- PlayersFileHandler.cs
+++ PlayersFileHandler.cs
@@ -1,26 +1,52 @@
 ﻿
 using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace BrainStorm
 {
     public class PlayersHandler
     {
         public List<Player> Players { get; set; } = new List<Player>();
+        public string PlayersFile { get; set; } = $"./Players.txt";
 
         public PlayersHandler()
         {
-            if (File.Exists($"./Players.txt") == false)
-                File.Create($"./Players.txt");
+            if (File.Exists(PlayersFile) == false)
+                File.Create(PlayersFile);
 
             else
             {
+                LoadPlayers();
+            }
+        }
 
+        public void SavePlayers()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(Players, options);
+            File.WriteAllText(PlayersFile, json);
+        }
+
+        public void LoadPlayers()
+        {
+            if (File.Exists(PlayersFile))
+            {
+                string json = File.ReadAllText(PlayersFile);
+                Players = JsonSerializer.Deserialize<List<Player>>(json) ?? new List<Player>();
+            }
+
+            else
+            {
+                Players = new List<Player>();
             }
         }
 
         public void AddPlayer(Player? player)
         {
-            throw new NotImplementedException();
+            if (player != null)
+                Players.Add(player);
         }
     }
 }
--- ScoresHandler.cs
+++ ScoresHandler.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace BrainStorm
 {
@@ -18,18 +21,31 @@ namespace BrainStorm
             }
         }
 
-        public void LoadScores()
-        {
-            
-        }
-
         public void SaveScores()
         {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(Scores, options);
+            File.WriteAllText(ScoresFile, json);
+        }
+
+        public void LoadScores()
+        {
+            if (File.Exists(ScoresFile))
+            {
+                string json = File.ReadAllText(ScoresFile);
+                Scores = JsonSerializer.Deserialize<List<TimeScorePair>>(json) ?? new List<TimeScorePair>();
+            }
+
+            else
+            {
+                Scores = new List<TimeScorePair>();
+            }
         }
 
         public void AddScore(TimeScorePair? score)
         {
-            throw new NotImplementedException();
+            if (score != null)
+                Scores.Add(score);
         }
     }
 }
